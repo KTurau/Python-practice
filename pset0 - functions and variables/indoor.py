ced5771ef26e5c76deae7f67e6b4099abd1e6e35
Kiryl Turau <71009434+KTurau@users.noreply.github.com>
2023-01-15 12:48:24 +0300

    Update indoor.py

--- pset0 - functions and variables/indoor.py
+++ pset0 - functions and variables/indoor.py
@@ -1,15 +1,9 @@
-# Implement a program in Python that prompts the user for input and then outputs that same input in lowercase. 
-# Punctuation and whitespace should be outputted unchanged. 
-# You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.
+# In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase. 
+# Punctuation and whitespace should be outputted unchanged. You’re welcome, but not required, to prompt the user explicitly, as by passing 
+# a str of your own as an argument to input.
 
 
 
-def main():
-    # Ask user input
-    src = str(input())
-    src = src.lower()
-    # Print in lowercase
-    print(src)
-
-main()
+some_input = input().lower()
+print(some_input)
 
