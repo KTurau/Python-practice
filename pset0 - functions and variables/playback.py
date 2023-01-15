a2b3a28b101cb815f054b148223280a780e4cada
Kiryl Turau <71009434+KTurau@users.noreply.github.com>
2023-01-15 11:42:25 +0300

    Create playback.py

---
+++ pset0 - functions and variables/playback.py
@@ -0,0 +1,10 @@
+# Implement a program in Python that prompts the user for input and then outputs that same input, replacing each space with ... (i.e., three periods).
+
+
+
+# Get input from the user
+msg = input()
+# Change whitespace for 3 dots
+new_msg = msg.replace(" ", "...")
+# Print output
+print(new_msg)
