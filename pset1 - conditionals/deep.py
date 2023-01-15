c73d4b0bf5e327b333cac6dfe2df34f0e2222c47
Kiryl Turau <71009434+KTurau@users.noreply.github.com>
2023-01-15 13:13:05 +0300

    Update deep.py

--- pset1 - conditionals/deep.py
+++ pset1 - conditionals/deep.py
@@ -1,13 +1,17 @@
-# Implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, 
+# In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything,
 # outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
 
 
-
-# Get user input
-answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ").lower().strip()
+answer = (
+    input(
+        "What is the answer to the Great Question of Life, the Universe and Everything? "
+    )
+    .lower()
+    .strip()
+)
 
 match answer:
-    case "42"|"forty two"|"forty-two":
+    case "42" | "forty two" | "forty-two":
         print("Yes")
     case _:
-        print("No") 
+        print("No")
